1042e57112ce0a2c2bb087149b339a70b0701813
Manny <104465644+scriptfree@users.noreply.github.com>
2024-11-25 18:03:20 -0800

    Update Zombie-Rush.lua

--- Zombie-Rush.lua
+++ Zombie-Rush.lua
@@ -1,1 +1,13 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Manny's Scripts", HidePremium = false, IntroEnabled = false, SaveConfig = nil})
+
+local teleportToggle = false
+local teleportPosition = CFrame.new(-744.01806640625, 54.25789642333984, -557.5186157226562)
+
+local Tab = Window:MakeTab({
+	Name = "Welcome",
+	Icon = "rbxassetid://17829948098",
+	PremiumOnly = false
+})
 
