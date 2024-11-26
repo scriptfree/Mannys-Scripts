1a013959ef599c57056725bf03680c24000f808c
Manny <104465644+scriptfree@users.noreply.github.com>
2024-11-25 18:03:02 -0800

    Update ISGGB.lua

--- ISGGB.lua
+++ ISGGB.lua
@@ -1,6 +1,6 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Manny's Script", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "Manny's Scripts", HidePremium = false, IntroEnabled = false, SaveConfig = nil})
 
 local teleportToggle = false
 local teleportPosition = CFrame.new(-744.01806640625, 54.25789642333984, -557.5186157226562)
